2f9417b5a70377da7b51fe32d151fce9590ce13c
Arseny Amerbaev <amerbaev@gmail.com>
2017-12-24 12:41:11 +0300

    графики

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from sklearn.preprocessing import StandardScaler, normalize
+from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.datasets import load_digits, load_breast_cancer, load_wine
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def kaiser(vals):
-    vals = list(filter(lambda x: x > 1, vals))
+    mean = sum(vals) / len(vals)
+    vals = list(filter(lambda x: x > mean, vals))
     return len(vals)
 
 
@@ -13,8 +16,8 @@ def broken_stick(vals):
     trC = sum(vals)
     n_comp = 0
     for i in range(n):
-        l = sum([1/j for j in range(i+1, n+1)])/n
-        if vals[i] / trC > l:
+        length = sum([1/j for j in range(i+1, n+1)])/n
+        if vals[i] / trC > length:
             n_comp += 1
         else:
             break
@@ -27,8 +30,18 @@ def process_dataset(x, name):
     x_cent = StandardScaler().fit_transform(x.data)
     pca = PCA(svd_solver='full')
     pca.fit(x_cent)
-    print('Главных компонент по правилу кайзера: ', kaiser(pca.explained_variance_))
-    print('Главных компонент по правилу сломанной трости: ', broken_stick(pca.explained_variance_), end='\n\n')
+    kaiser_size = kaiser(pca.explained_variance_)
+    broken_size = broken_stick(pca.explained_variance_)
+    print('Главных компонент по правилу кайзера: ', kaiser_size)
+    print('Главных компонент по правилу сломанной трости: ', broken_size, end='\n\n')
+
+    plt.xlabel('Номер компоненты')
+    plt.ylabel('% дисперсии')
+    plt.bar(range(len(pca.explained_variance_ratio_)), pca.explained_variance_ratio_ * 100)
+    plt.scatter(kaiser_size, pca.explained_variance_ratio_[kaiser_size] * 100)
+    plt.scatter(broken_size, pca.explained_variance_ratio_[broken_size] * 100)
+    sns.set()
+    plt.show()
 
 
 if __name__ == '__main__':
